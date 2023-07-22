cc21bd60384b3f124f60514d76625109ac136a80
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-07-22 02:30:06 +0200

    intermediate revision - segregated the exception throwing and plain tests in CommonsCommandLineParser

--- src/test/java/example/CommonsCommandLineParserTest.java
+++ src/test/java/example/CommonsCommandLineParserTest.java
@@ -16,40 +16,14 @@ import junit.framework.TestCase;
  * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
  */
 
-@SuppressWarnings("deprecation")
+@SuppressWarnings({ "deprecation", "serial" })
 public class CommonsCommandLineParserTest extends TestCase {
 
 	private CommonsCommandLineParser sut;
 
-	protected static class ExitException extends SecurityException {
-		public final int status;
-
-		public ExitException(int status) {
-			super("intercepted exit");
-			this.status = status;
-		}
-	}
-
-	private static class NoExitSecurityManager extends SecurityManager {
-		@Override
-		public void checkPermission(Permission perm) {
-		}
-
-		@Override
-		public void checkPermission(Permission perm, Object context) {
-		}
-
-		@Override
-		public void checkExit(int status) {
-			super.checkExit(status);
-			throw new ExitException(status);
-		}
-	}
-
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		System.setSecurityManager(new NoExitSecurityManager());
 		sut = new CommonsCommandLineParser();
 		sut.saveFlagValue("d", "data");
 		sut.saveFlagValue("o", "operation");
@@ -57,106 +31,38 @@ public class CommonsCommandLineParserTest extends TestCase {
 
 	@Override
 	protected void tearDown() throws Exception {
-		System.setSecurityManager(null);
 		super.tearDown();
-	}
-
-	@Test
-	public void test1() throws Exception {
-		System.out.println("test passes");
-	}
-
-	@Test
-	public void test2() throws Exception {
-		final int setStatus = 43;
-		try {
-			sut.help(setStatus);
-		} catch (ExitException e) {
-			final int status = e.status;
-			assertThat(String.format("Unexpected Exit status", status), status,
-					is(setStatus));
-		}
-	}
-
-	@Test
-	public void test3() throws Exception {
-		try {
-			sut.run("-d", "data (required) here", "-o", "operation (required) here");
-		} catch (ExitException e) {
-			final int status = e.status;
-			assertThat(String.format("Unexpected Exit status", status), status,
-					is(0));
-		}
-	}
-
-	@Test
-	public void test4() throws Exception {
-		try {
-			sut.run("-a", "wrong option");
-		} catch (ExitException e) {
-			final int status = e.status;
-			assertThat(String.format("Unexpected Exit status", status), status,
-					is(CommonsCommandLineParser.INVALID_OPTION));
-		}
-	}
-
-	@Test
-	// missing required option
-	public void test5() throws Exception {
-		try {
-			sut.run("-d", "data (required) here");
-		} catch (ExitException e) {
-			final int status = e.status;
-			assertThat(String.format("Unexpected Exit status %d", status), status,
-					is(CommonsCommandLineParser.INVALID_OPTION));
-		}
 	}
 
 	@Test
 	// handling url
 	public void test6() throws Exception {
-		try {
-			String data = "http://www.google.com";
-			sut.run("-d", data, "-o", "operation");
-			String value = sut.getFlagValue("d");
-			assertThat(String.format("Unexpected value %s", value), value, is(data));
-		} catch (ExitException e) {
-			final int status = e.status;
-			assertThat(String.format("Unexpected Exit status", status), status,
-					is(0));
-		}
+		String data = "http://www.google.com";
+		sut.run("-d", data, "-o", "operation");
+		String value = sut.getFlagValue("d");
+		assertThat(String.format("Unexpected value %s", value), value, is(data));
 	}
 
 	@Test
 	// handling long option names
-	// NOTE: the test has to catch exitexception, even there is no real exception
-	// TODO: the option is not working
+	// TODO: the option is not working ?
 	public void test7() throws Exception {
-		try {
-			sut.run("--data", "data", "--o", "operation");
-			String value = sut.getFlagValue("d");
-			assertThat(String.format("Unexpected value %s", value), value,
-					is("data"));
-		} catch (ExitException e) {
-			final int status = e.status;
-			assertThat(String.format("Unexpected Exit status %d", status), status,
-					is(42));
-		}
+		sut.run("--data", "data", "--o", "operation");
+		String value = sut.getFlagValue("d");
+		assertThat(String.format("Unexpected value %s", value), value, is("data"));
 	}
 
 	@Test
-	// handling long option names
-	// NOTE: the test has to catch exitexception, even there is no real exception
 	public void test8() throws Exception {
-		try {
-			sut.run("--data", "data", "-d", "different data");
-			String value = sut.getFlagValue("d");
-			assertThat(String.format("Unexpected value %s", value), value,
-					is("data"));
-		} catch (ExitException e) {
-			final int status = e.status;
-			assertThat(String.format("Unexpected Exit status %d", status), status,
-					is(42));
-		}
+		sut.run("--data", "data", "-d", "different data");
+		String value = sut.getFlagValue("d");
+		assertThat(String.format("Unexpected value %s", value), value, is("data"));
+	}
+
+	public void test9() throws Exception {
+		sut.run("--data", "a,b,c,d,e,f", "--operaiton", "a,b,c,d,e,f");
+		String[] v1 = sut.getFlagValue("d").split(",");
+		String[] v2 = sut.getFlagValue("o").split(",");
+		assertThat(v1.length == v2.length, is(true));
 	}
 }
